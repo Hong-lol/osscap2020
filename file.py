7199f94559deab209137b15ab8fadc65d2efe24d
Hong-lol <72370753+Hong-lol@users.noreply.github.com>
2020-11-29 01:30:06 +0900

    Add files via upload

--- file.py
+++ file.py
@@ -4,18 +4,18 @@ import time
 delay = 0.000001
 
 GPIO.setmode(GPIO.BCM)
-red1_pin = 17
-green1_pin = 18
-blue1_pin = 22
-red2_pin = 23
-green2_pin = 24
-blue2_pin = 25
-clock_pin = 3
-a_pin = 7
-b_pin = 8
-c_pin = 9
+red1_pin = 11
+green1_pin = 27
+blue1_pin = 7
+red2_pin = 8
+green2_pin = 9
+blue2_pin = 10
+clock_pin = 17
+a_pin = 22
+b_pin = 23
+c_pin = 24
 latch_pin = 4
-oe_pin = 2
+oe_pin = 18
 
 GPIO.setup(red1_pin, GPIO.OUT)
 GPIO.setup(green1_pin, GPIO.OUT)
@@ -95,19 +95,19 @@ def set_pixel(x, y, color):
     screen[y][x] = color
 
 def number0(x, y):
-    for x in range(3):
+    for a in range(3):
         screen[y][x+2]=1
         screen[y+7][x+2]=1
         x+=1
     x-=3
-    for y in range(6):
+    for b in range(6):
         screen[y+1][x+1]=1
         screen[y+1][x+5]=1
         y+=1
     
     
 def number1(x, y):
-    for y in range(8):
+    for a in range(8):
         screen[y][x+3]=1
         y+=1
     y-=8
@@ -116,24 +116,24 @@ def number1(x, y):
     screen[y+7][x+4]=1
     
 def number2(x, y):
-    for x in range (3):
+    for a in range (3):
         screen[y][x+2]=1
         x+=1
     x-=3
     screen[y+1][x+1]=1
-    for y in range (3):
+    for b in range (3):
         screen[y+1][x+5]=1
         y+=1
     y-=3
     screen[y+4][x+4]=1
     screen[y+5][x+3]=1
     screen[y+6][x+2]=1
-    for x in range (5):
+    for c in range (5):
         screen[y+7][x+1]=1
         x+=1
 
 def number3(x, y):
-    for x in range (3):
+    for a in range (3):
         screen[y][x+2]=1
         screen[y+3][x+2]=1
         screen[y+7][x+2]=1
@@ -143,16 +143,16 @@ def number3(x, y):
     screen[y+1][x+5]=1
     screen[y+2][x+5]=1
     screen[y+6][x+1]=1
-    for y in range (3):
+    for b in range (3):
         screen[y+4][x+5]=1
         y+=1
 
 def number4(x, y):
-    for y in range(8):
+    for a in range(8):
         screen[y][x+5]=1
         y+=1
     y-=8
-    for x in range(5):
+    for b in range(5):
         screen[y+5][x+1]=1
         x+=1
     x-=5
@@ -162,19 +162,19 @@ def number4(x, y):
     screen[y+4][x+1]=1
         
 
-def number5(x, y):
-    for x in range (3):
+def number5(a,b):
+    for a in range (3):
         screen[y][x+2]=1
         screen[y+3][x+2]=1
         screen[y+7][x+2]=1
         x+=1
     x-=3
     screen[y][x+5]=1
-    for y in range(4):
+    for b in range(4):
         screen[y][x+1]=1
         y+=1
     y-=4
-    for y in range(3):
+    for c in range(3):
         screen[y+4][x+5]=1
         y+=1
     y-=3
@@ -182,13 +182,13 @@ def number5(x, y):
     
 
 def number6(x, y):
-    for x in range(3):
+    for a in range(3):
         screen[y][x+2]=1
-        screen[y+3][x]=1
-        screen[y+7][x]=1
+        screen[y+3][x+2]=1
+        screen[y+7][x+2]=1
         x+=1
     x-=3
-    for y in range(6):
+    for b in range(6):
         screen[y+1][x+1]=1
         screen[y+1][x+5]=1
         y+=1
@@ -197,11 +197,11 @@ def number6(x, y):
     screen[y+3][x+5]=0
 
 def number7(x, y):
-    for x in range(5):
+    for a in range(5):
         screen[y][x+1]=1
         x+=1
     x-=5
-    for y in range(3):
+    for b in range(3):
         screen[y][x+5]=1
         screen[y+5][x+2]=1
         y+=1
@@ -210,35 +210,135 @@ def number7(x, y):
     screen[y+4][x+3]=1
 
 def number8(x, y):
-    for x in range (3):
+    for a in range (3):
         screen[y][x+2]=1
         screen[y+3][x+2]=1
         screen[y+7][x+2]=1
         x+=1
     x-=3
-    for y in range (6):
+    for b in range (6):
         screen[y+1][x+1]=1
         screen[y+1][x+5]=1
         y+=1
     y-=6
     screen[y+3][x+1]=0
-    screen[y+3][x+5]=1
+    screen[y+3][x+5]=0
     
 
 def number9(x, y):
-    for x in range(3):
+    for a in range(3):
         screen[y][x+2]=1
         screen[y+4][x+2]=1
         screen[y+7][x+2]=1
         x+=1
     x-=3
-    for y in range(6):
+    for b in range(6):
         screen[y+1][x+1]=1
         screen[y+1][x+5]=1
         y+=1
     y-=6
     screen[y+4][x+1]=0
     screen[y+5][x+1]=0
+
+while True:
+    turget=input("목표 속도":)
+
+    if turget[0]==1:
+        number1(8,0)
+
+    elif turget[0]==2:
+        number2(8,0)
+
+    elif turget[0]==3:
+        number3(8,0)
+
+    elif turget[0]==4:
+        number4(8,0)
+
+    elif turget[0]==5:
+        number5(8,0)
+
+    elif turget[0]==6:
+        number6(8,0)
+
+    elif turget[0]==7:
+        number7(8,0)
+
+    elif turget[0]==8:
+        number8(8,0)
+
+    elif turget[0]==9:
+        number9(8,0)
+
+    else :
+        number0(8,0)
+
+
+
+
+
+    if turget[1]==1:
+        number1(16,0)
+
+    elif turget[1]==2:
+        number2(16,0)
+
+    elif turget[1]==3:
+        number3(16,0)
+
+    elif turget[1]==4:
+        number4(16,0)
+
+    elif turget[1]==5:
+        number5(16,0)
+
+    elif turget[1]==6:
+        number6(16,0)
+
+    elif turget[1]==7:
+        number7(16,0)
+
+    elif turget[1]==8:
+        number8(16,0)
+
+    elif turget[1]==9:
+        number9(16,0)
+
+    else :
+        number0(16,0)
+
+
+
+    if turget[2]==1:
+        number1(24,0)
+
+    elif turget[2]==2:
+        number2(24,0)
+
+    elif turget[2]==3:
+        number3(24,0)
+
+    elif turget[2]==4:
+        number4(24,0)
+
+    elif turget[2]==5:
+        number5(24,0)
+
+    elif turget[2]==6:
+        number6(24,0)
+
+    elif turget[2]==7:
+        number7(24,0)
+
+    elif turget[2]==8:
+        number8(24,0)
+
+    elif turget[2]==9:
+        number9(24,0)
+    else :
+        number0(0,0)
+
+
 
 
 while True:
